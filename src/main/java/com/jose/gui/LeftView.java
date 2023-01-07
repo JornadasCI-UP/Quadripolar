beb533362c6a4c16e194fc2011b26bb2ef308e15
Jose Ribeiro <josealbertoribeiro800@gmail.com>
2023-01-07 16:07:02 +0000

    LeftView UI construction

--- src/main/java/com/jose/gui/LeftView.java
+++ src/main/java/com/jose/gui/LeftView.java
@@ -1,8 +1,54 @@
 package com.jose.gui;
 
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
 
 public class LeftView extends VBox {
     
-    Pane connection, attendance;
+    private TitledPane connection, attendance;
+    private TextField presencial, online;
+    private int numPres, numOnline = 0;
+
+    public LeftView(){
+        buildConnection();
+        buildAttendance();
+        this.getChildren().addAll(connection, attendance);
+    }
+
+    private void buildConnection(){
+
+        VBox connectionBox = new VBox();
+        TextArea username;
+        PasswordField password;
+        Button connect;
+
+        connect = new Button("Ligar");
+        username = new TextArea();
+        password = new PasswordField();
+        connectionBox.getChildren().addAll(username, password, connect);
+        connection = new TitledPane("Ligação",connectionBox);
+        connection.setCollapsible(false);
+
+    }
+
+    private void buildAttendance(){
+        
+        VBox attendanceBox = new VBox();
+
+        presencial = new TextField(String.format("Numero de participantes presenciais %d", numPres)); 
+        online = new TextField(String.format("Numero de participantes online %d", numOnline));
+        attendanceBox.getChildren().addAll(presencial, online);
+
+        attendance = new TitledPane("Participantes", attendanceBox);
+        attendance.setCollapsible(false);
+
+    }
+
+    public void setNumPres(int num){
+        numPres = num;
+    }
+
+    public void setNumRemote(int num){
+        numOnline = num;
+    }
 }
--- src/main/java/com/jose/gui/mainview.java
+++ src/main/java/com/jose/gui/mainview.java
@@ -6,10 +6,13 @@ public class mainview extends BorderPane {
     
     ScrollView scroll;
     SearchPane search;
+    LeftView left;
 
     public mainview(){
         search = new SearchPane();
         scroll = new ScrollView();
+        left = new LeftView();
+        this.setLeft(left);
         this.setTop(search);
         this.setCenter(scroll);
     }
